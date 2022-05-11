cf30516be7a117137d0717f552fb5cdb0f159026
kkranzfelder <kkranzfelder@gmail.com>
2022-05-11 14:40:09 -0400

    Started laying out skeletal foundation for server and building out DB Models.

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,8 +1,33 @@
 const mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost:3000/glossary');
 
-const
+const {Schema} = mongoose;
 
+const entrySchema = new Schema({
+  id: Number,
+  word: String,
+  definition: String,
+  createdAt: String,
+});
+
+const Entry = mongoose.model('Entry', entrySchema);
+
+updateAndSave = (entry) => {
+  Promise.all(
+    findOneAndUpdate(
+      {
+        {word: entry.word}
+      },
+      {
+
+      },
+      {
+
+      }
+    )
+  )
+
+}
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -7,13 +7,25 @@ const app = express();
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+app.get('/', (req, res) => {
+  //get all data from DB
+});
+
+app.get('/entry', (req, res) => {
+  //get specific entry
+});
+
+app.post('/entry', (req, res) => {
+  //post new word/definition
+});
+
+app.put('/entry', (req, res) => {
+  //update entry in database
+});
+
+app.delete('/entry', (req, res) => {
+  //delete entry
+})
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
