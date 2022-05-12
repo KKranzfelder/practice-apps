a410a2b2c85c40789102500af2e3d934c0b03ca7
kkranzfelder <kkranzfelder@gmail.com>
2022-05-12 19:23:24 -0400

    Search component built out and am starting testing for functionality.

--- 1-glossary/client/src/Search.jsx
+++ 1-glossary/client/src/Search.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 class Search extends React.Component {
+  constructor(props) {
+    this.state = {
+      value: ''
+    }
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({
+      value: event.target.value
+    })
+    this.props.display(this.state.value);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Search saved terms:
+          <input
+          type='text'
+          value={this.state.value}
+          placeholder='Search saved terms...'
+          />
+        </label>
+        <input type='submit' value='Submit'/>
+      </form>
+    )
+  }
 }
 
 export default Search;
--- 1-glossary/client/src/index.jsx
+++ 1-glossary/client/src/index.jsx
@@ -4,14 +4,17 @@ import ReactDOM from "react-dom";
 import axios from "axios";
 import Glossary from "./Glossary.jsx";
 import AddWord from "./AddWord.jsx";
+import Search from "./Search.jsx"
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      words: []
+      words: [],
+      displayedWords:[]
     }
     this.fetchData = this.fetchData.bind(this);
+    this.setDisplay = this.setDisplay.bind(this);
   }
 
   fetchData(endpoint) {
@@ -21,8 +24,22 @@ class App extends React.Component {
         console.log('response data within fetchData: ', res.data);
         this.setState({
           words: res.data,
+          displayedWords: res.data
         });
       });
+  }
+
+  setDisplay(input) {
+    let matchedWords = [];
+    this.state.words.forEach((word) => {
+      if(word.word.includes(input)) {
+        matchedWords.push(word);
+      }
+    })
+    this.setState({
+      words: this.state.words,
+      displayedWord: matchedWords
+    });
   }
 
   componentDidMount() {
@@ -36,6 +53,7 @@ class App extends React.Component {
     return (
       <div>
         <AddWord fetch={this.fetchData}/>
+        <Search words={this.state.words} display={this.setDisplay}/>
         <Glossary
         words={this.state.words}
         fetch={this.fetchData}/>
